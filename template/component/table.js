dfa9ded0384a9f1d53193db0ba67736eeac279fc
GitSBE-it01 <it.01@sbspeaker.com>
2023-11-22 06:54:15 +0700

    21 nov 23 tambahan

---
+++ template/component/table.js
@@ -0,0 +1,112 @@
+/*===================================================
+jig table generate
+===================================================*/
+export const tHeader = async(target,tableID, arrHead, color) => {
+    try {    
+        const container = document.getElementById(target);
+        const table = document.createElement('div');
+        table.classList.add('tm');
+        table.id = tableID;
+        const tr = document.createElement('div');
+        tr.classList.add('fr', 'fc-ctr', 'fc-w', 'pr4', 'thCont');
+        tr.id = 'tableHeader';
+
+        for (let i=0; i<arrHead.length; i++) {
+            const th = document.createElement('div');
+            th.classList.add('flexCh', 'th', 'bd-black', 'upper', color);
+            th.textContent = arrHead[i];
+            tr.appendChild(th);
+        }
+        table.appendChild(tr);
+        container.appendChild(table);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+export const tData = async (tbodyID, data, arr, color) => {
+    try {
+        const tableBody = document.createElement('div');
+        tableBody.classList.add('tr');
+        tableBody.id = tbodyID;
+        for (let i=0; i<data.length; i++) {
+            const tr2 = document.createElement('div');
+            tr2.setAttribute('data-tr', '');
+            tr2.classList.add('fr', 'pr1');
+            for (let ii=0; ii<arr.length; ii++) {
+                const tData = document.createElement('div');
+                tData.classList.add('flexCh', 'td', 'bd-black', `${color}`, 'cap');
+                tData.textContent = `${data[i].arr[i]}`;
+                tr2.appendChild(tData);
+            }
+            tableBody.appendChild(tr2);
+        }
+        return tableBody;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const tDataInput = async (tbodyID, data, arr, color, noteData) => {
+    try {
+        const tableBody = document.createElement('div');
+        tableBody.classList.add('tr');
+        tableBody.id = tbodyID;
+        for (let i=0; i<data.length; i++) {
+            const tr2 = document.createElement('div');
+            tr2.setAttribute('data-tr', '')
+            tr2.classList.add('fr', 'pr1');
+            for (let ii=0; ii<arr.length; ii++) {
+                const tData = document.createElement('div');
+                tData.classList.add('flexCh', 'td', 'bd-black', `${color}`, 'cap');
+                const tInput = document.createElement('input');
+                tInput.classList.add(`${color}`)
+                tInput.value = data[i][arr[ii]];
+                tInput.setAttribute('type','text');
+                tInput.setAttribute('data-input', noteData);
+                tInput.id = arr[ii]+ data[i][arr[ii]];;
+                tData.appendChild(tInput);
+                tr2.appendChild(tData);
+            }
+            tableBody.appendChild(tr2);
+        }
+        return tableBody;
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const insertHidInp = async (target, data, arr) => {
+    try {
+        const dataTr = document.querySelectorAll(target);
+        for (let i=0; i<dataTr.length; i++) {
+            for (let ii=0; ii<arr.length; ii++) {
+                const input = document.createElement('input');
+                input.value = `${data[i].arr[ii]}`;
+                input.setAttribute('type','hidden');
+                input.setAttribute('data-input', arr[ii]);
+                input.id = `${arr[ii]}+${data[i]}`;
+                dataTr.appendChild(input);
+            }
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const insertHidDiv = async (target, data, arr) => {
+    try {
+        const dataTr = document.querySelectorAll(target);
+        for (let i=0; i<dataTr.length; i++) {
+            for (let ii=0; ii<arr.length; ii++) {
+                const div = document.createElement('div');
+                div.classList.add('hide');
+                div.setAttribute('data-div', arr[ii]);
+                div.id = `${arr[ii]}+${data[i]}`;
+                dataTr.appendChild(div);
+            }
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
---
+++ template/index.php
@@ -0,0 +1,27 @@
+<?php
+require_once "config.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/layout.css">
+    <link rel="stylesheet" href="../assets/css/animation.css">
+    <link rel="stylesheet" href="../assets/css/font.css">
+    <link rel="stylesheet" href="../assets/css/color.css">
+    <link rel="stylesheet" href="../assets/css/table.css">
+    <link rel="stylesheet" href="../assets/css/search_btn.css">
+    <title>Document</title>
+</head>
+<body>
+<input type='hidden' value="<?php echo $role; ?>" id='role'>
+<div id='root'></div>
+
+<script type='module'>
+    
+</script>
+<script src="../assets/template/library/sheetjs/xlsx.full.min.js"></script>
+</body>
+</html>
